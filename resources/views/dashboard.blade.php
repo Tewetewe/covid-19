0529f1da6013673d406202074bf1591b3fff23ac
Tewetewe <trywiguna@gmail.com>
2020-06-17 21:45:54 +0800

    fix last

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1202,11 +1202,11 @@
                                             <td>{{ $i }}</td>
                                             <td>{{ $global[$i-1 + (sizeof($global)-186)]->OBJECTID }}</td>
                                             <td align="right">{{ number_format($global[$i-1 + (sizeof($global)-186)]->Confirmed,0,",",",") }}</td>
-                                            <td align="right">{{ number_format((($global[$i-1 + (sizeof($global)-186)]->Confirmed)-($global[$i-1]->Confirmed))/(sizeof($global)/186),2,",",",") }}</td>
+                                            <td align="right">{{ number_format((($global[$i-1 + (sizeof($global)-186)]->Confirmed)-($global[$i-1]->Confirmed))/(sizeof($global)/186),0,",",",") }}</td>
                                             <td align="right">{{ number_format($global[$i-1 + (sizeof($global)-186)]->Recovered,0,",",",") }}</td>
-                                            <td align="right">{{ number_format(($global[$i-1 + (sizeof($global)-186)]->Recovered/($global[$i-1 + (sizeof($global)-186)]->Confirmed)*100),2) }} %</td>
+                                            <td align="right">{{ number_format(($global[$i-1 + (sizeof($global)-186)]->Recovered/($global[$i-1 + (sizeof($global)-186)]->Confirmed)*100),2) }}</td>
                                             <td align="right">{{ number_format($global[$i-1 + (sizeof($global)-186)]->Deaths,0,",",",") }}</td>
-                                            <td align="right">{{ number_format(($global[$i-1 + (sizeof($global)-186)]->Deaths/($global[$i-1 + (sizeof($global)-186)]->Confirmed)*100),2) }} %</td>
+                                            <td align="right">{{ number_format(($global[$i-1 + (sizeof($global)-186)]->Deaths/($global[$i-1 + (sizeof($global)-186)]->Confirmed)*100),2) }}</td>
                                         </tr>
                                     @endfor
                                 </tbody>
@@ -1245,11 +1245,11 @@
                                             <td>{{ $i }}</td>
                                             <td>{{ $provinsi[$i-1 + (sizeof($provinsi)-34)]->FID }}</td>
                                             <td align="right">{{ number_format($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi,0,",",",") }}</td>
-                                            <td align="right">{{ number_format((($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi)-($provinsi[$i-1]->Kasus_Posi))/(sizeof($provinsi)/34),2,",",",") }}</td>
+                                            <td align="right">{{ number_format((($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi)-($provinsi[$i-1]->Kasus_Posi))/(sizeof($provinsi)/34),0,",",",") }}</td>
                                             <td align="right">{{ number_format($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Semb,0,",",",") }}</td>
-                                            <td align="right">{{ number_format(($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Semb/($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi)*100),2) }} %</td>
+                                            <td align="right">{{ number_format(($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Semb/($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi)*100),2) }}</td>
                                             <td align="right">{{ number_format($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Meni,0,",",",") }}</td>
-                                            <td align="right">{{ number_format(($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Meni/($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi)*100),2) }} %</td>
+                                            <td align="right">{{ number_format(($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Meni/($provinsi[$i-1 + (sizeof($provinsi)-34)]->Kasus_Posi)*100),2) }}</td>
                                              
                                         </tr>
                                     @endfor
